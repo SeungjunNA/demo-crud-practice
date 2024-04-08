cab09007ac71aaf2993b45487cc5cb428c914bd3
SeungjunNA <nsjjsng1@gmail.com>
2024-04-08 18:26:24 +0900

    샘플 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 INSERT INTO member (id, name) VALUES (1, 'name1')
 INSERT INTO member (id, name) VALUES (2, 'name2')
 INSERT INTO member (id, name) VALUES (3, 'name3')
+
+-- article 데이터 생성
+INSERT INTO article(title, content) VALUES('제목1', '내용1')
+INSERT INTO article(title, content) VALUES('제목2', '내용2')
+INSERT INTO article(title, content) VALUES('제목3', '내용3')
